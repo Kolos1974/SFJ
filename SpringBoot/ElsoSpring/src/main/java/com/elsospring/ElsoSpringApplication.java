9d6d0ad5d23c87dd9e984b9cd1b5b185f98b1407
Kolos <karlovitz.kolos@gmail.com>
2024-06-04 14:21:58 +0200

    12. Csoportos külső konfiguráció

--- SpringBoot/ElsoSpring/src/main/java/com/elsospring/ElsoSpringApplication.java
+++ SpringBoot/ElsoSpring/src/main/java/com/elsospring/ElsoSpringApplication.java
@@ -5,11 +5,13 @@ import java.util.Arrays;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@EnableConfigurationProperties
 @ComponentScan({"com.elsospring","com.spy"})
 @SpringBootApplication
 //@Configuration
@@ -17,10 +19,12 @@ import org.springframework.context.annotation.Configuration;
 public class ElsoSpringApplication {
 
 	
+	/*
 	@Bean
 	public Person giveMeAPerson() {
 		return new Person("Gyula", 20);
 	}
+	*/
 	
 	
 	
@@ -28,6 +32,8 @@ public class ElsoSpringApplication {
 	public static void main(String[] args) {
 		ApplicationContext ct = SpringApplication.run(ElsoSpringApplication.class, args);
 		
+		
+		System.out.println(ct.getBean("person"));
 		
 		/*
 		String[] beanArray = ct.getBeanDefinitionNames();
--- SpringBoot/ElsoSpring/src/main/java/com/elsospring/Person.java
+++ SpringBoot/ElsoSpring/src/main/java/com/elsospring/Person.java
@@ -1,25 +1,48 @@
 package com.elsospring;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@ConfigurationProperties(prefix = "person")
+@Component
 public class Person {
-	private String name;
+	private String firstName;
+	private String lastName;
 	private int age;
+	private String address;
+	private String phoneNumber;
+	
 	
 	
 	public Person() {
+
+	}
+	
+	
+	
+	
+	
+	public Person(String firstName, String lastName, int age, String address, String phoneNumber) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+		this.address = address;
+		this.phoneNumber = phoneNumber;
 	}
 
-	public Person(String name, int age) {
-		super();
-		this.name = name;
-		this.age = age;
+
+	public String getFirstName() {
+		return firstName;
 	}
-	
-	
-	public String getName() {
-		return name;
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
 	}
-	public void setName(String name) {
-		this.name = name;
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
 	}
 	public int getAge() {
 		return age;
@@ -27,6 +50,29 @@ public class Person {
 	public void setAge(int age) {
 		this.age = age;
 	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Person [firstName=" + firstName + ", lastName=" + lastName + ", age=" + age + ", address=" + address
+				+ ", phoneNumber=" + phoneNumber + "]";
+	}
+	
+	
+	
 	
 
 }
